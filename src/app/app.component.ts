7e34cf6925cfbaba18bb272868357bf0b226d553
jmethukten <thukten73@gmail.com>
2022-07-07 10:30:08 -0400

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, VERSION } from '@angular/core';
+import { Component, OnDestroy, OnInit, VERSION } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Person } from './interface';
 import { PersonService } from './person.service';
 
@@ -7,17 +8,24 @@ import { PersonService } from './person.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   person: Person[] = [];
+  personSubscription: Subscription;
   constructor(private personService: PersonService) {}
 
   ngOnInit() {
-    this.personService.person$.subscribe((personJson: any) => {
-      this.person = [...JSON.parse(personJson)];
-    });
+    this.personSubscription = this.personService.person$.subscribe(
+      (personJson: any) => {
+        this.person = [...JSON.parse(personJson)];
+      }
+    );
   }
 
   deletePerson(id: number) {
     this.personService.deletePerson(id);
+  }
+
+  ngOnDestroy() {
+    this.personSubscription.unsubscribe();
   }
 }
